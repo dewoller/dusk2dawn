dddb0278e4ac6dd398a40ab5e7d4c9ec793954ff
Dennis Wollersheim <dewoller@gmail.com>
2019-07-01 15:09:47 +1000

    first draft

--- lib/get_data.R
+++ lib/get_data.R
@@ -7,7 +7,7 @@ my_db_write <- function ( df, table_name ) {
   drv <- dbDriver("PostgreSQL")
   con <- dbConnect(drv, dbname = my_db_name,
                    host = "localhost", port = 5432,
-                   user = "dewoller", password = key_get('mofi', 'dewoller'))
+                   user = "dewoller", password = Sys.getenv('PASSWD'))
   on.exit(dbDisconnect(con))
   dbWriteTable( con, table_name, df )
 }
@@ -18,7 +18,7 @@ my_db_append <- function ( df, table_name ) {
   drv <- dbDriver("PostgreSQL")
   con <- dbConnect(drv, dbname = my_db_name,
           host = "localhost", port = 5432,
-          user = "dewoller", password = key_get('mofi', 'dewoller'))
+          user = "dewoller", password = Sys.getenv('PASSWD'))
   on.exit(dbDisconnect(con))
   dbWriteTable( con, table_name, df, append=TRUE )
 }
@@ -31,9 +31,11 @@ my_db_read <- function ( query ) {
   drv <- dbDriver("PostgreSQL")
   con <- dbConnect(drv, dbname = my_db_name,
           host = "localhost", port = 5432,
-          user = "dewoller", password = key_get('mofi', 'dewoller'))
+          user = "dewoller", password = Sys.getenv('PASSWD'))
   on.exit(dbDisconnect(con))
   dbGetQuery( con, query )
 
 }
+
+
 # -------------------------------------------------
