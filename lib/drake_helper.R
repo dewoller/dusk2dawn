3b901fb35eb9b46fffd6e08c31f3fa3dfaa57928
Dennis Wollersheim <dewoller@gmail.com>
2019-12-03 12:20:37 +1100

    Helpful drake functions

---
+++ lib/drake_helper.R
@@ -0,0 +1,13 @@
+
+stub="df_matching_survey"
+n=1
+readd_first_like = function( stub, n=1 ) {
+
+  cached() %>% 
+    enframe() %>%
+    filter( startsWith( value, stub )) %>%
+    head(n) %>%
+    tail(1) %>%
+    purrr::pluck("value") %>% 
+    readd( character_only=TRUE)
+}
