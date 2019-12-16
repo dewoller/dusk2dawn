616da4eeaf6452341a63e532a969e337bda97aa6
Dennis Wollersheim <dewoller@gmail.com>
2019-12-16 14:07:15 +1100

    cleanup testing code

--- lib/optics_find_staypoint.R
+++ lib/optics_find_staypoint.R
@@ -1,5 +1,112 @@
 library(futile.logger)
 
+if(FALSE) {
+
+df_location %>%
+  head(1) %>%
+  select(userid, night) %>%
+  inner_join( df_location) %>%
+  select( longitude, latitude, timestamp) %>% 
+  { . } -> traj
+
+options(error=recover)
+options(error=stop)
+
+stdbscan( traj, 50/111320, 3600/2, 100) -> a
+
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+
+traj %>%
+  normalize <- function(x) {
+    return ((x - min(x)) / (max(x) - min(x)))
+  }
+
+ df_location %>%
+   distinct( userid, night ) %>%
+           head(2) %>%
+           tail(1) %>%
+
+readd( interpolated_locations_120_filtered_accuracy_100 ) %>%
+  inner_join( df_location %>%
+             distinct( userid, night ) %>%
+             head(4) %>%
+               #tail(1) %>%
+               select(userid, night) ) %>% 
+               { . } -> b
+
+cache %>% 
+  filter(endsWith(value, 'interpolated_locations_120_filtered_accuracy_100'))
+
+
+b %>%
+#  arrange(timestamp) %>%
+  select( longitude, latitude) %>% 
+  mutate_all(normalize) %>% 
+  { . } -> x
+x %>%
+  optics( ) %>% 
+  { . } -> a
+
+extractXi(a, .5) ->a; a$clusters_xi
+hullplot(x, a)
+
+
+res <- extractDBSCAN(a, eps_cl = .065)
+res
+plot(res)  ## black is noise
+hullplot(x, res)
+
+### re-cut at a higher eps threshold
+res <- extractDBSCAN(res, eps_cl = .01)
+res
+plot(res)
+hullplot(x, res$cluster)
+
+
+### extract hierarchical clustering of varying density using the Xi method
+res <- extractXi(res, xi = 0.2)
+res
+plot(res)
+hullplot(x, res)
+
+
+
+b %>%
+  mutate( cluster = res$cluster ) %>% 
+  { . } -> d
+
+a
+
+d %>%
+  filter( cluster > 0 ) %>%
+  ggplot( aes( latitude, longitude, color=as.factor(cluster))) +
+  geom_point()
+
+d %>%
+  filter( cluster > 0 ) %>%
+  mutate( 
+         m_lat = ll2m( latitude, min(latitude), m_per_latitude),
+         m_lon = ll2m( longitude, min(longitude), m_per_longitude)) %>%
+  ggplot( aes( m_lat, m_lon, color=as.factor(cluster))) +
+  geom_point()
+
+
+# greedy algorithm;  extract the longest group of points that fits within the constrained area.  Repeat
+
+b
+do1( b, .0001, 5,1000)
+
+a=tibble()
+for ( eps_cl in rev(1:100/1000)) {
+  a %>% bind_rows( do1( b, eps_cl, min_staypoint_time = 10, max_staypoint_distance = 500 )   )  %>% 
+  { . } -> a
+        }
+}
+
+
 # testing codde starts here
 
 if(FALSE) {
